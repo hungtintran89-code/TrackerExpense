73388c0008ce9d21a8ea58f17460e0313c2d7c6c
HungTin <hungtintran89@gmail.com>
2026-07-16 16:20:39 +0700

    fix : ignore....

--- src/main/java/database/DBConnection.java
+++ src/main/java/database/DBConnection.java
@@ -7,9 +7,10 @@ public class DBConnection {
 
     public Connection getConnection(){
         try{
-            return DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/postgres",
-                                                    "postgres" ,
-                                                            "12345") ;
+            String url = System.getenv("DB_URL") != null ? System.getenv("DB_URL") : "jdbc:postgresql://127.0.0.1:5432/postgres";
+            String user = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "postgres";
+            String password = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : "12345";
+            return DriverManager.getConnection(url, user, password);
         }catch( Exception e ){
             e.printStackTrace() ;
             return null ;
